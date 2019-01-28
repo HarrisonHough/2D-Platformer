dcd7769915a08197d69ef57265924df6353a826b
Harrison Hough <harrisonhough4@gmail.com>
2019-01-28 11:29:24 +1100

    Recreated project

---
+++ Scripts/TimeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    private float startTime = 0;
+    public float StartTime { get { return startTime; } }
+    private float endTime = 0;
+
+    public void SetStartTime()
+    {
+            startTime = Time.time;
+    }
+
+    public void SetEndTime()
+    {
+        endTime = Time.time;
+    }
+
+    public float GetFinalTime()
+    {
+         return endTime - startTime;
+    }
+
+
+
+}
